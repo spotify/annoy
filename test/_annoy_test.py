c6f18eb280a9b4fc0e11ce1db09d1014a7fe7759
Erik Bernhardsson <erikbern@spotify.com>
2013-04-01 15:02:01 +0000

    tests for euclidean distance + some large index tests

--- test/_annoy_test.py
+++ test/_annoy_test.py
@@ -12,10 +12,14 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+# TeamCity fails to run this test because it can't import the C++ module.
+# I think it's because the C++ part gets built in another directory.
+
 import unittest
+import random
 from annoy import AnnoyIndex
 
-class IndexTest(unittest.TestCase):
+class AngularIndexTest(unittest.TestCase):
     def test_get_nns_by_vector(self):
         f = 3
         i = AnnoyIndex(f)
@@ -37,10 +41,63 @@ class IndexTest(unittest.TestCase):
         self.assertEquals(i.get_nns_by_item(0, 3), [0,1,2])
         self.assertEquals(i.get_nns_by_item(1, 3), [1,0,2])
 
-    def test_cos(self):
+    def test_dist(self):
         f = 2
         i = AnnoyIndex(f)
         i.add_item(0, [0, 1])
         i.add_item(1, [1, 1])
 
-        self.assertAlmostEqual(i.get_cos(0, 1), 2 ** -0.5)
+        self.assertAlmostEqual(i.get_distance(0, 1), 1.0 - 2 ** -0.5)
+
+    def test_large_index(self):
+        # Generate pairs of random points where the pair is super close
+        f = 10
+        i = AnnoyIndex(f)
+        for j in xrange(0, 10000, 2):
+            p = [random.gauss(0, 1) for z in xrange(f)]
+            f1 = random.random() + 1
+            f2 = random.random() + 1
+            x = [f1 * pi + random.gauss(0, 1e-2) for pi in p]
+            y = [f2 * pi + random.gauss(0, 1e-2) for pi in p]
+            i.add_item(j, x)
+            i.add_item(j+1, y)
+        
+        i.build(10)
+        for j in xrange(0, 10000, 2):
+            self.assertEquals(i.get_nns_by_item(j, 2), [j, j+1])
+            self.assertEquals(i.get_nns_by_item(j+1, 2), [j+1, j])
+
+
+class EuclideanIndexTest(unittest.TestCase):
+    def test_get_nns_by_vector(self):
+        f = 2
+        i = AnnoyIndex(f, 'euclidean')
+        i.add_item(0, [2,2])
+        i.add_item(1, [3,2])
+        i.build(10)
+
+        self.assertEquals(i.get_nns_by_vector([3,3], 2), [1, 0])
+
+    def test_dist(self):
+        f = 2
+        i = AnnoyIndex(f, 'euclidean')
+        i.add_item(0, [0, 1])
+        i.add_item(1, [1, 1])
+
+        self.assertAlmostEqual(i.get_distance(0, 1), 1.0)
+
+    def test_large_index(self):
+        # Generate pairs of random points where the pair is super close
+        f = 10
+        i = AnnoyIndex(f, 'euclidean')
+        for j in xrange(0, 10000, 2):
+            p = [random.gauss(0, 1) for z in xrange(f)]
+            x = [pi + random.gauss(0, 1e-2) for pi in p]
+            y = [pi + random.gauss(0, 1e-2) for pi in p]
+            i.add_item(j, x)
+            i.add_item(j+1, y)
+        
+        i.build(10)
+        for j in xrange(0, 10000, 2):
+            self.assertEquals(i.get_nns_by_item(j, 2), [j, j+1])
+            self.assertEquals(i.get_nns_by_item(j+1, 2), [j+1, j])
