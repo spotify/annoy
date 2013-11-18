b93c0a425fc1548257553f1ec90d48fdda2e6023
Radim Řehůřek <radimrehurek@seznam.cz>
2013-11-18 23:23:11 +0100

    fix build on OS X

--- src/annoylib.cc
+++ src/annoylib.cc
@@ -12,6 +12,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+#include "Python.h"
 #include <stdio.h>
 #include <string>
 #include <boost/python.hpp>
