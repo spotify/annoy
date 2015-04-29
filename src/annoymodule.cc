7b3d0f73c86521f5ea5dc274457e3a542bbd28d5
Maxim Bulatov <dvenum.at@gmail.com>
2015-04-30 02:04:49 +0600

    * expanding the tabs in PyTypeObject

--- src/annoymodule.cc
+++ src/annoymodule.cc
@@ -439,44 +439,44 @@ static PyMethodDef AnnoyMethods[] = {
 
 static PyTypeObject PyAnnoyType = {
   PyObject_HEAD_INIT(NULL)
-  0,						 /*ob_size*/
-  "annoy.Annoy",			 /*tp_name*/
-  sizeof(py_annoy),			 /*tp_basicsize*/
-  0,						 /*tp_itemsize*/
+  0,                      /*ob_size*/
+  "annoy.Annoy",          /*tp_name*/
+  sizeof(py_annoy),       /*tp_basicsize*/
+  0,                      /*tp_itemsize*/
   (destructor)py_an_dealloc, /*tp_dealloc*/
-  0,						 /*tp_print*/
-  0,						 /*tp_getattr*/
-  0,						 /*tp_setattr*/
-  0,						 /*tp_compare*/
-  0,						 /*tp_repr*/
-  0,						 /*tp_as_number*/
-  0,						 /*tp_as_sequence*/
-  0,						 /*tp_as_mapping*/
-  0,						 /*tp_hash */
-  0,						 /*tp_call*/
-  0,						 /*tp_str*/
-  0,						 /*tp_getattro*/
-  0,						 /*tp_setattro*/
-  0,						 /*tp_as_buffer*/
+  0,                      /*tp_print*/
+  0,                      /*tp_getattr*/
+  0,                      /*tp_setattr*/
+  0,                      /*tp_compare*/
+  0,                      /*tp_repr*/
+  0,                      /*tp_as_number*/
+  0,                      /*tp_as_sequence*/
+  0,                      /*tp_as_mapping*/
+  0,                      /*tp_hash */
+  0,                      /*tp_call*/
+  0,                      /*tp_str*/
+  0,                      /*tp_getattro*/
+  0,                      /*tp_setattro*/
+  0,                      /*tp_as_buffer*/
   Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-  "annoy objects",			 /* tp_doc */
-  0,						 /* tp_traverse */
-  0,						 /* tp_clear */
-  0,						 /* tp_richcompare */
-  0,						 /* tp_weaklistoffset */
-  0,						 /* tp_iter */
-  0,						 /* tp_iternext */
-  AnnoyMethods,				 /* tp_methods */
-  py_annoy_members,			 /* tp_members */
-  0,						 /* tp_getset */
-  0,						 /* tp_base */
-  0,						 /* tp_dict */
-  0,						 /* tp_descr_get */
-  0,						 /* tp_descr_set */
-  0,						 /* tp_dictoffset */
-  (initproc)py_an_init,		 /* tp_init */
-  0,						 /* tp_alloc */
-  py_an_new,				 /* tp_new */
+  "annoy objects",        /* tp_doc */
+  0,                      /* tp_traverse */
+  0,                      /* tp_clear */
+  0,                      /* tp_richcompare */
+  0,                      /* tp_weaklistoffset */
+  0,                      /* tp_iter */
+  0,                      /* tp_iternext */
+  AnnoyMethods,           /* tp_methods */
+  py_annoy_members,       /* tp_members */
+  0,                      /* tp_getset */
+  0,                      /* tp_base */
+  0,                      /* tp_dict */
+  0,                      /* tp_descr_get */
+  0,                      /* tp_descr_set */
+  0,                      /* tp_dictoffset */
+  (initproc)py_an_init,   /* tp_init */
+  0,                      /* tp_alloc */
+  py_an_new,              /* tp_new */
 };
 
 static PyMethodDef module_methods[] = {
