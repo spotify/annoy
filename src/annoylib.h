9defbcb3fbef4b905f4e15611fb101255a6c9e23
Erik Bernhardsson <erikbern@spotify.com>
2015-11-22 22:19:54 -0500

    stdint.h too

--- src/annoylib.h
+++ src/annoylib.h
@@ -22,18 +22,18 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stddef.h>
-#include <stdint.h>
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 #include <io.h>
-#else
-#include <unistd.h>
-#endif
-
-#ifdef __MINGW32__
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 #include "mman.h"
 #include <windows.h>
 #else
+#include <unistd.h>
+#include <stdint.h>
 #include <sys/mman.h>
 #endif
 
