1a8865dde855ef3ad9756fa9e1166e1d35c09d18
Erik Bernhardsson <erikbern@spotify.com>
2016-07-17 21:27:49 -0400

    unit test examples too

---
+++ test/examples_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+def simple_test():
+    execfile('examples/simple_test.py')
+
+def mmap_test():
+    execfile('examples/mmap_test.py')
+
+def precision_test():
+    execfile('examples/precision_test.py')
