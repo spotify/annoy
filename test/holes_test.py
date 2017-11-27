7597afc4389c27c15231822b7a24cc6c7f5d35af
Romain Yon <romain@spotify.com>
2017-11-27 18:31:50 -0500

    fix holes test

--- test/holes_test.py
+++ test/holes_test.py
@@ -30,12 +30,10 @@ class HolesTest(TestCase):
     def test_holes_more(self):
         f = 10
         index = AnnoyIndex(f)
-        valid_indices = set()
-        for i in range(1000):
-            i2 = int(i*2**-0.5) # leave holes every few items
-            valid_indices.add(i2)
+        valid_indices = range(0, 2000, 2) # leave holes
+        for i in valid_indices:
             v = numpy.random.normal(size=(f,))
-            index.add_item(i2, v)
+            index.add_item(i, v)
         index.build(10)
         for i in valid_indices:
             js = index.get_nns_by_item(i, 10000)
