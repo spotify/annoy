23e5c501db63e842a41091349d079e7434bbffbf
Erik Bernhardsson <mail@erikbern.com>
2018-01-19 23:48:39 -0500

    version 1.11.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ else:
     cputune = []
 
 setup(name='annoy',
-      version='1.10.0',
+      version='1.11.1',
       description='Approximate Nearest Neighbors in C++/Python optimized for memory usage and loading/saving to disk.',
       packages=['annoy'],
       ext_modules=[
