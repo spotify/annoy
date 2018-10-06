40161e38781cf7495b74f52b61489f82ca1e6531
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-06 17:43:09 +0100

    Inlined all non-static mman functions.

--- src/mman.h
+++ src/mman.h
@@ -85,7 +85,7 @@ static DWORD __map_mmap_prot_file(const int prot)
     return desiredAccess;
 }
 
-void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
+inline void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 {
     HANDLE fm, h;
     
@@ -156,7 +156,7 @@ void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
     return map;
 }
 
-int munmap(void *addr, size_t len)
+inline int munmap(void *addr, size_t len)
 {
     if (UnmapViewOfFile(addr))
         return 0;
@@ -166,7 +166,7 @@ int munmap(void *addr, size_t len)
     return -1;
 }
 
-int mprotect(void *addr, size_t len, int prot)
+inline int mprotect(void *addr, size_t len, int prot)
 {
     DWORD newProtect = __map_mmap_prot_page(prot);
     DWORD oldProtect = 0;
@@ -179,7 +179,7 @@ int mprotect(void *addr, size_t len, int prot)
     return -1;
 }
 
-int msync(void *addr, size_t len, int flags)
+inline int msync(void *addr, size_t len, int flags)
 {
     if (FlushViewOfFile(addr, len))
         return 0;
@@ -189,7 +189,7 @@ int msync(void *addr, size_t len, int flags)
     return -1;
 }
 
-int mlock(const void *addr, size_t len)
+inline int mlock(const void *addr, size_t len)
 {
     if (VirtualLock((LPVOID)addr, len))
         return 0;
@@ -199,7 +199,7 @@ int mlock(const void *addr, size_t len)
     return -1;
 }
 
-int munlock(const void *addr, size_t len)
+inline int munlock(const void *addr, size_t len)
 {
     if (VirtualUnlock((LPVOID)addr, len))
         return 0;
