5ef5dd6ddc3010b06e8c8e2717e4a3126a377317
Erik Bernhardsson <mail@erikbern.com>
2020-02-29 15:22:39 -0500

    inline helper functions

--- src/annoylib.h
+++ src/annoylib.h
@@ -81,7 +81,7 @@ typedef signed __int64    int64_t;
   # include <alloca.h>
 #endif
 
-void set_error_from_errno(char **error, const char* msg) {
+inline void set_error_from_errno(char **error, const char* msg) {
   showUpdate("%s: %s (%d)\n", msg, strerror(errno), errno);
   if (error) {
     *error = (char *)malloc(256);  // TODO: win doesn't support snprintf
@@ -89,7 +89,7 @@ void set_error_from_errno(char **error, const char* msg) {
   }
 }
 
-void set_error_from_string(char **error, const char* msg) {
+inline void set_error_from_string(char **error, const char* msg) {
   showUpdate("%s\n", msg);
   if (error) {
     *error = (char *)malloc(strlen(msg) + 1);
