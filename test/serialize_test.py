4661f319e5d1424b8d649a50a626c41283a0fb63
FuexFollets <fuex9follets@gmail.com>
2024-01-24 17:50:30 -0500

    Added serialization test

---
+++ test/serialize_test.py
@@ -0,0 +1,41 @@
+import random
+
+from annoy import AnnoyIndex
+
+def test_serialize_index():
+    f = 32
+
+    index = AnnoyIndex(f, 'angular')
+
+    for iteration in range(1000):
+        vector = [random.gauss(0, 1) for z in range(f)]
+        index.add_item(iteration, vector)
+
+    index.build(10)
+
+    _ = index.serialize()
+
+
+def test_deserialize_index():
+    f = 32
+
+    index = AnnoyIndex(f, 'angular')
+
+    for iteration in range(1000):
+        vector = [random.gauss(0, 1) for z in range(f)]
+        index.add_item(iteration, vector)
+
+    index.build(10)
+
+    data = index.serialize()
+
+    index2 = AnnoyIndex(f, 'angular')
+
+    index2.deserialize(data)
+
+    index_item_count = index.get_n_items()
+
+    assert index_item_count == index2.get_n_items()
+    assert index.get_n_trees() == index2.get_n_trees()
+    assert index.get_nns_by_item(0, index_item_count) == index2.get_nns_by_item(0, index_item_count)
+
