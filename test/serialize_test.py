f7df982b1031cb0465042db6959ad773aee0a165
FuexFollets <fuex9follets@gmail.com>
2024-01-28 11:03:33 -0500

    Added test for serialization on index mmaped from file

--- test/serialize_test.py
+++ test/serialize_test.py
@@ -8,7 +8,7 @@ def test_serialize_index():
     index = AnnoyIndex(f, 'angular')
 
     for iteration in range(1000):
-        vector = [random.gauss(0, 1) for z in range(f)]
+        vector = [random.gauss(0, 1) for _ in range(f)]
         index.add_item(iteration, vector)
 
     index.build(10)
@@ -22,7 +22,7 @@ def test_deserialize_index():
     index = AnnoyIndex(f, 'angular')
 
     for iteration in range(1000):
-        vector = [random.gauss(0, 1) for z in range(f)]
+        vector = [random.gauss(0, 1) for _ in range(f)]
         index.add_item(iteration, vector)
 
     index.build(10)
@@ -38,4 +38,23 @@ def test_deserialize_index():
     assert index_item_count == index2.get_n_items()
     assert index.get_n_trees() == index2.get_n_trees()
     assert index.get_nns_by_item(0, index_item_count) == index2.get_nns_by_item(0, index_item_count)
+
+def test_serialize_after_load():
+    f = 32
+
+    index1 = AnnoyIndex(f, 'angular')
+
+    for iteration in range(1000):
+        vector = [random.gauss(0, 1) for _ in range(f)]
+        index1.add_item(iteration, vector)
+
+    index1.build(10)
+
+    save_path = "test/test.tree"
+    index1.save(save_path)
+
+    index2 = AnnoyIndex(f, 'angular')
+    index2.load(save_path)
+
+    assert index1.serialize() == index2.serialize()
 
